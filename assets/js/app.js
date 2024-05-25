9d33560f8b855aacfee467573f72ead3c3e1eb65
pjvillasista <pjvillasista@gmail.com>
2024-05-24 20:03:38 -0700

    Easier Navbar editts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuIcon = document.querySelector(".menu-icon");
     const navbar = document.querySelector(".navbar");
-    
-    menuIcon.addEventListener("click", function() {
-        navbar.classList.toggle("translate-x-0");
-        navbar.classList.toggle("-translate-x-full");
-    });
+
+    if (menuIcon && navbar) {
+        menuIcon.addEventListener("click", function() {
+            navbar.classList.toggle("translate-x-0");
+            navbar.classList.toggle("-translate-x-full");
+        });
+    }
 
     const closeBtn = document.querySelector(".close-btn");
     if (closeBtn) {
@@ -16,5 +18,17 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     // Initialize highlight.js
-    hljs.highlightAll();
+    if (window.hljs) {
+        hljs.highlightAll();
+    }
+
+    // Load the navbar dynamically
+    fetch('../navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            const navbarPlaceholder = document.getElementById('navbar-placeholder');
+            if (navbarPlaceholder) {
+                navbarPlaceholder.innerHTML = data;
+            }
+        });
 });
